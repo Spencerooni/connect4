45d95748768b5b2aa7d169dd0d5345116ae24aae
Corey Duffy <c.duffy@kainos.com>
2016-10-13 12:52:41 +0100

    Adding basic constrints

--- Connect4.sql
+++ Connect4.sql
@@ -2,9 +2,11 @@ CREATE DATABASE Connect4;
 USE Connect4;
 SET AUTO_COMMIT = 0;
 
+DROP TABLE IF EXISTS Logins;
+
 CREATE TABLE Logins
 (
-username varchar(50),
-password varchar(50),
-accessNo int
+username varchar(50) NOT NULL PRIMARY KEY,
+password varchar(50) NOT NULL,
+accessNo int NOT NULL
 );
